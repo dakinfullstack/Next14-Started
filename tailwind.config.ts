6a81cd200db28f290640b37cab4f0da761c5a14f
dakin fullstack <dakin.fullstack@gmail.com>
2024-05-09 16:47:04 +0700

    Start Project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,22 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+ daisyui: {
+  themes: [
+    {
+      mytheme: {"primary": "#009fe3",
+                "secondary": "#1f9cd6",
+"accent": "#a4e2f7",
+"neutral": "#f6fdff",
+"base-100": "#fffdf1",
+"info": "#2daae2",
+"success": "#1ad1a5",
+"warning": "#ff9900",
+"error": "#ff003f",
       },
     },
+  ],
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 };
 export default config;
